cea1b0dff3717565b368cb5cfa583875c60546c5
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2022-05-04 08:51:53 -0300

    Ajuste nova tabela de preços

--- element/aboutUs-2.js
+++ element/aboutUs-2.js
@@ -1,36 +1,67 @@
 import Link from "next/link";
 
 function AboutUs_2() {
-    return (
-      <>
-        {/* <!-- About us --> */}
-		<section className="content-inner about-wraper-1" style={{"backgroundImage":"url(images/background/bg15.png)","backgroundSize":"contain","backgroundPosition":"center right","backgroundRepeat":"no-repeat"}}>
-			<div className="container">
-				<div className="row align-items-center">
-					<div className="col-lg-6 m-b30 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.2s">
-						<div className="dz-media left">
-							<img src="images/about/pic1.jpg" alt=""/>
-						</div>
-					</div>
-					<div className="col-lg-6 m-b30 wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.4s">
-						<div className="section-head style-3">
-							<h6 className="sub-title text-primary bgl-primary m-b15">O QUE IRÁ APRENDER?</h6>
-							<h2 className="title m-b20">Bem vindo a uma experiência Racing, que vai acelerar suas vendas.</h2>
-							<p>Proin laoreet leo vel enim gravida, at porttitor metus ultricies. Cras eu velit enim. Vivamus blandit, dolor ut aliquet rutrum, ex elit mattis sapien, non molestie felis neque et nulla. Sed euismod turpis id nibh suscipit semper. Pellentesque dapibus risus arcu.</p>
-						</div>
-						<ul className="list-check primary m-b30">
-							<li>Suspendisse ullamcorper mollis orci in facilisis.</li>
-							<li>Etiam orci magna, accumsan varius enim volutpat.</li>
-							<li>Donec fringilla velit risus, in imperdiet turpis euismod quis.</li>
-							<li>Aliquam pulvinar diam tempor erat pellentesque, accumsan mauri.</li>
-						</ul>
-						<Link href="about-us-2"><a className="btn btn-primary rounded-xl gradient">Learn More</a></Link>
-					</div>
-				</div>
-			</div>
-		</section>
-      </>
-    )
-  }
-  
-  export default AboutUs_2;
+  return (
+    <>
+      {/* <!-- About us --> */}
+      <section
+        className="content-inner about-wraper-1"
+        style={{
+          backgroundImage: "url(images/background/bg15.png)",
+          backgroundSize: "contain",
+          backgroundPosition: "center right",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="container">
+          <div className="row align-items-center">
+            <div
+              className="col-lg-6 m-b30 wow fadeInLeft"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div className="dz-media left">
+                <img src="images/about/pic1_2.jpg" alt="" />
+              </div>
+            </div>
+            <div
+              className="col-lg-6 m-b30 wow fadeInRight"
+              data-wow-duration="2s"
+              data-wow-delay="0.4s"
+            >
+              <div className="section-head style-3">
+                <h6 className="sub-title text-primary bgl-primary m-b15">
+                  Plano Hastighet
+                </h6>
+                <h2 className="title m-b20">
+                  Valor promocional de <s>R$499,00</s> por apenas R$49,90!
+                </h2>
+                <p>
+                  Bem vindo a uma experiência Racing, que vai acelerar suas
+                  vendas.
+                </p>
+              </div>
+              <ul className="list-check primary m-b30">
+                <li>
+                  Suporte com equipe especializada para tirar suas duvidas
+                </li>
+                <li>
+                  Náo gostou? Sem problema, seu dinheiro de volta em até 7 dias
+                </li>
+                <li>Aulas explicativas em video com apostilas em PDF</li>
+                <li>Atualizações de conteúdos por até 1 ano</li>
+              </ul>
+              
+                <a href="https://app-vlc.hotmart.com/products/bw/manage/1969489" target="_blank" className="btn btn-primary rounded-xl gradient">
+                  Adquira hoje mesmo!
+                </a>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default AboutUs_2;
